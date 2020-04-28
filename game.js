7961f3469e1a670d848e6a5e30e9f860d626d536
AnushaUnni <36429841+AnushaUnni@users.noreply.github.com>
2020-04-28 19:27:34 +0530

    inital commit

---
+++ game.js
@@ -0,0 +1,79 @@
+var buttonColours = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).on("keypress",function(event){
+    if(!started){
+        $("#level-title").text("Level "+ level);
+        nextSequence(event);
+        started = true;
+    }
+});
+
+function nextSequence(){
+    userClickedPattern=[];
+    level++;
+    $("#level-title").text("Level "+ level);
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    console.log(audio.getAttribute("src"));
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("."+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("."+currentColour).removeClass("pressed");
+    },100);
+}
+
+function gameOver(){
+    $("body").addClass("game-over");
+    setTimeout(function(){
+        $("body").removeClass("game-over");
+    },200);
+}
+
+function startOver(){
+    started = false;
+    level = 0;
+    gamePattern = [];
+}
+$(".btn").click(function(){
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+ }); 
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+        console.log("success");
+        if(userClickedPattern.length == gamePattern.length){
+            setTimeout(function(){
+                nextSequence()
+             },1000);
+        } 
+    }
+    else{
+   
+   playSound("wrong");
+    gameOver();
+    startOver();
+    }
+}
+
+
+
+
